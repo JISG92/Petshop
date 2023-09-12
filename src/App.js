b36bc64caead7eec7c72ee1cb6407a2118fa26a7
Jovan <jovansanchez9212@gmail.com>
2023-09-11 23:11:53 -0500

    Curso React Sesion 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
-import Home from "./pages/Home"
-import Sobre from "./pages/Sobre"
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <Home />
-      <Sobre />
-    </>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sobre" element={<Sobre />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
