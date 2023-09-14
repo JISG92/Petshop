ac51bd2cc13afc6d196f4ed010afb4bb25bcebca
Jovan <jovansanchez9212@gmail.com>
2023-09-13 21:54:49 -0500

    sesion 2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./assets/css/base/base.css";
 import "./assets/css/componentes/card.css";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+import Page404 from "./pages/page404";
+import Header from "./components/Headers";
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
 	return (
 		<Router>
+			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/sobre" element={<Sobre />} />
+				<Route path="*" element={<Page404 />} />
 			</Routes>
 		</Router>
 	);
