155b0a3d4d6dae9b9e816a10e85ecb0ba54986c4
JISG92 <123202079+JISG92@users.noreply.github.com>
2023-09-20 22:34:23 -0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: "https://github.com/JISG92/Petshop/tree/main/db.json",
+	baseURL: "main/db.json",
 });
 
 export const buscar = async (url, setData) => {
